c428a8bb5a05b8429b03b7b514dc5c101d90bce8
宋毅 <31555603+sonyi2009@users.noreply.github.com>
2018-10-07 16:42:30 +0800

    TCP 服务器
    c# 下的tcp通讯，服务器的简单demo案例，之后优化为异步获取

---
+++ Server_01/Client_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client_01
+{
+    class Program
+    {
+        private static Socket clientSocket = null;
+        static void Main(string[] args)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+            e.UserToken = clientSocket;
+            e.RemoteEndPoint = remoteEP;
+            clientSocket.ConnectAsync(e);
+            while (true) { }
+        }
+    }
+}
---
+++ Server_01/Server_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server_01
+{
+    class Program
+    {
+        private static Socket serverSocket = null;
+        static void Main(string[] args)
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 9999);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                IPEndPoint ip = (IPEndPoint)clientSocket.RemoteEndPoint;
+                Console.WriteLine(ip.Port.ToString());
+                Thread thread = new Thread(listenClientConnect);
+                thread.Start(clientSocket);
+            }
+        }
+        private static void listenClientConnect(object obj)
+        {
+            Socket clientSocket = obj as Socket;
+            //Socket clientSocket = serverSocket.Accept();
+            clientSocket.Send(Encoding.Default.GetBytes("你可以的啊"));
+            Thread recThread = new Thread(ReceiveClientMessage);
+            recThread.Start(clientSocket);
+        }
+
+        private static void ReceiveClientMessage(object clientSocket)
+        {
+            Socket socket = clientSocket as Socket;
+            byte[] buffer = new byte[1024];
+            int length = socket.Receive(buffer);
+            Console.WriteLine(Encoding.Default.GetString(buffer,0, length));
+        }
+    }
+}
